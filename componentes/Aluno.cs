2b069dc3a51ab7ec404c49b3fdc0cebba4d7efcf
renato_sans <renato@datacopy.com.br>
2021-11-22 05:19:49 -0300

    Commit inicial do projeto

---
+++ componentes/Aluno.cs
@@ -0,0 +1,10 @@
+namespace PortalAlunoWASM
+{
+    // using System.Text.Json.Serialization;
+
+    public class Aluno
+    {
+        public int id { get; set; }
+        public String nome { get; set; }
+    }
+}
