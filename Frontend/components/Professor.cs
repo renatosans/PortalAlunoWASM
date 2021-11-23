faeb1b1d1dc03c8eecc23b8f983d93cd0ce09258
renato_sans <renato@datacopy.com.br>
2021-11-23 19:57:12 -0300

    Ajustes

---
+++ Frontend/components/Professor.cs
@@ -0,0 +1,11 @@
+﻿namespace PortalAlunoWASM.components
+{
+    public class Professor
+    {
+        public int id { get; set; }
+        public String nome { get; set; }
+        public String email { get; set; }
+        public Byte[] foto { get; set; }
+        public String formatoImagem { get; set; }
+    }
+}
