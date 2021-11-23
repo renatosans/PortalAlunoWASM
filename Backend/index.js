ac0da70dfe75abec8352f9122d3a78dc002d8d3a
renato_sans <renato@datacopy.com.br>
2021-11-23 11:11:53 -0300

    Ajustes

--- Backend/index.js
+++ Backend/index.js
@@ -6,3 +6,49 @@ import express from "express";
 var app = express();
 const staticRoot = '../Frontend';
 const port = 3000;
+
+// configura os parametros de conexão
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'p@ssw0rd',
+    database: 'portal_aluno'
+});
+
+// conecta ao banco de dados
+mysqlConnection.connect(
+    err => {
+        if (!err) {
+            console.log("DB connection succeeded!");
+        } else {
+            console.log("DB connection failed!\n Error: " + JSON.stringify(err, undefined, 2));
+        }
+    }
+);
+
+app.use("/", express.static(staticRoot));
+
+// inicia a API escutando na porta 3000
+app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
+
+// recupera o cadastro
+app.get('/recuperarProfessor/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM professor WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log("Error: " + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
+
+// lista todos os registros da tabela
+app.get('/listarProfessores', (req, res) => {
+    mysqlConnection.query('SELECT * FROM professor', (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        } else {
+            console.log("Error: " + JSON.stringify(err, undefined, 2));
+        }
+    })
+});
