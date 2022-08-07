20652cbb21354230d0402ddac31369dfb9956e5d
renato_sans <renato@datacopy.com.br>
2022-08-07 03:10:41 -0300

    Ajustes

--- Frontend/components/Aluno.cs
+++ Frontend/components/Aluno.cs
@@ -3,6 +3,6 @@ namespace PortalAlunoWASM
     public class Aluno
     {
         public int id { get; set; }
-        public String nome { get; set; }
+        public String? nome { get; set; }
     }
 }
