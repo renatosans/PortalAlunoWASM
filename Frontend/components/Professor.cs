2b2070a5c9497234704478d0db9e87d9495e18d9
renato_sans <renato@datacopy.com.br>
2022-08-07 03:58:18 -0300

    Ajustes

--- Frontend/components/Professor.cs
+++ Frontend/components/Professor.cs
@@ -17,5 +17,27 @@ namespace PortalAlunoWASM.components
             JsonElement element = (JsonElement)foto; // element.Deserialize(...
             return element.ToString();
         }
+
+        /*
+        public void InsereFoto() {
+            // Para fazer o upload para o banco de dados é necessário aumentar o tamanho do packet
+            // SET GLOBAL max_allowed_packet = 850741824
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() != DialogResult.OK) return;
+
+            String filename = Path.GetFileNameWithoutExtension(fileDialog.FileName);
+            Byte[] fileContents = File.ReadAllBytes(fileDialog.FileName);
+            String extension = Path.GetExtension(fileDialog.FileName); extension = extension.Trim('.');
+
+
+            String query = "UPDATE `portal_aluno`.`professor` SET `foto` = '" + Convert.ToBase64String(fileContents) + "', `formatoImagem` = 'image/" + extension + ";base64' WHERE id = " + filename;
+            MySqlCommand command = new MySqlCommand(query, mySqlConnection);
+            int rowsAffected = command.ExecuteNonQuery();
+
+            MessageBox.Show(rowsAffected + " registro(s) foram inseridos no Banco de Dados");
+        }
+        */
     }
+
 }
